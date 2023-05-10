de0f5ea80ad30b3bc4c92a67f5f953d5ba3b2e15
zhixian0123 <10160765@me.mcu.edu.tw>
2023-05-10 12:00:53 +0800

    add week13

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -4,39 +4,81 @@
 ///把它從Desktop\freeglut\bin改成.(現在目錄的意思)DEBUG和Release都要設定才會將.cbp檔存檔
 #include <stdio.h>
 #include <GL/glut.h>
+#include "glm.h"///week13 step02-1
+GLMmodel * head = NULL;///week13 step02-1
+GLMmodel * body = NULL;
+GLMmodel * uparmR = NULL;
+GLMmodel * lowarmR = NULL;
+int show[4] = {0,1,0,0};
+void keyboard(unsigned char key ,int x, int y)
+{
+    if(key=='0') show[0] = !show[0];///week13 step03-1
+    if(key=='1') show[1] = !show[1];
+    if(key=='2') show[2] = !show[2];
+    if(key=='3') show[3] = !show[3];
+    glutPostRedisplay();
+}
 FILE * fout = NULL;///step02-2一開始檔案沒有開,NULL
 FILE * fin = NULL;
-float teapotX=0,teapotY=0;
+float teapotX=0, teapotY=0;
+float angle=0, angle2=0, angle3=0;
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
-        glTranslatef(teapotX,teapotY,0);
-        glutSolidTeapot(0.3);
+        ///glTranslatef(teapotX,teapotY,0);
+        ///glutSolidTeapot(0.3);
+        glScalef(0.2,0.2,0.2);///week13 step02-2
+        if(body==NULL)///week13 step02-1
+        {
+            head = glmReadOBJ("model/head.obj");
+            body = glmReadOBJ("model/body.obj");///week13 step02-1
+            uparmR = glmReadOBJ("model/uparmR.obj");
+            lowarmR = glmReadOBJ("model/lowarmR.obj");
+            ///glmUnitize(body);///week13 step02-1
+        }
+        if(show[0]) glmDraw(head,GLM_MATERIAL);///week13 step03-1
+        if(show[1]) glmDraw(body,GLM_MATERIAL);
+        glPushMatrix();
+            glTranslatef(teapotX, teapotY, 0);
+            if(show[2]) glmDraw(uparmR,GLM_MATERIAL);
+        glPopMatrix();
+        if(show[3]) glmDraw(lowarmR,GLM_MATERIAL);
     glPopMatrix();
     glutSwapBuffers();
+}
+int oldX = 0, oldY = 0;
+void motion(int x, int y)
+{
+    teapotX += (x - oldX)/150.0;
+    teapotY -= (y - oldY)/150.0;
+    oldX = x;
+    oldY = y;
+    printf("glTranslatef(%f, %f, 0);\n", teapotX, teapotY);
+    glutPostRedisplay();
 }
 void mouse(int button, int state,int x, int y)
 {
     if(state==GLUT_DOWN)
     {
-        teapotX = (x-150)/150.0;
-        teapotY = (150-y)/150.0;
-        if(fout==NULL) fout = fopen("file4.txt", "w");
-        fprintf(fout, "%f %f\n", teapotX, teapotY);
+        oldX = x;
+        oldY = y;
+        angle = x;
+        ///if(fout==NULL) fout = fopen("file4.txt", "w");
+        ///fprintf(fout, "%f %f\n", teapotX, teapotY);
     }
     display();
 }
-void keyboard(unsigned char key, int x, int y)
-{
-    if(fin==NULL)
-    {
-        fclose(fout);
-        fin = fopen("file4.txt","r");
-    }
-    fscanf(fin, "%f %f", &teapotX, &teapotY);
-    display();
-}
+///void keyboard(unsigned char key, int x, int y)
+///{
+    ///if(fin==NULL)
+    ///{
+        ///fclose(fout);
+        ///fin = fopen("file4.txt","r");
+    ///}
+    ///fscanf(fin, "%f %f", &teapotX, &teapotY);
+    ///display();
+///}
 int main(int argc, char ** argv)
 {
     glutInit(&argc, argv);
@@ -45,6 +87,7 @@ int main(int argc, char ** argv)
 
 	glutDisplayFunc(display);
 	glutMouseFunc(mouse);
+	glutMotionFunc(motion);
 	glutKeyboardFunc(keyboard);
 	glutMainLoop();
 }
