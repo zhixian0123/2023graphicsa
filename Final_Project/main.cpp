f1cb351324aa833ecf57a52d5984e541847a9d7b
zhixian0123 <10160765@me.mcu.edu.tw>
2023-05-17 11:21:42 +0800

    week14挑選關節

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -9,13 +9,18 @@ GLMmodel * head = NULL;///week13 step02-1
 GLMmodel * body = NULL;
 GLMmodel * uparmR = NULL;
 GLMmodel * lowarmR = NULL;
-int show[4] = {0,1,0,0};
+int show[4] = {1,1,1,1};
+int ID = 0;///0:頭  1:身體  2:上手臂  3:下手臂
 void keyboard(unsigned char key ,int x, int y)
 {
-    if(key=='0') show[0] = !show[0];///week13 step03-1
-    if(key=='1') show[1] = !show[1];
-    if(key=='2') show[2] = !show[2];
-    if(key=='3') show[3] = !show[3];
+    if(key=='0') ID = 0;
+    if(key=='1') ID = 1;
+    if(key=='2') ID = 2;
+    if(key=='3') ID = 3;
+    ///if(key=='0') show[0] = !show[0];///week13 step03-1
+    ///if(key=='1') show[1] = !show[1];
+    ///if(key=='2') show[2] = !show[2];
+    ///if(key=='3') show[3] = !show[3];
     glutPostRedisplay();
 }
 FILE * fout = NULL;///step02-2一開始檔案沒有開,NULL
@@ -28,7 +33,7 @@ void display()
     glPushMatrix();
         ///glTranslatef(teapotX,teapotY,0);
         ///glutSolidTeapot(0.3);
-        glScalef(0.2,0.2,0.2);///week13 step02-2
+        glScalef(0.2, 0.2, 0.2);///week13 step02-2
         if(body==NULL)///week13 step02-1
         {
             head = glmReadOBJ("model/head.obj");
@@ -37,12 +42,23 @@ void display()
             lowarmR = glmReadOBJ("model/lowarmR.obj");
             ///glmUnitize(body);///week13 step02-1
         }
+        if(ID==0) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[0]) glmDraw(head,GLM_MATERIAL);///week13 step03-1
+
+        if(ID==1) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[1]) glmDraw(body,GLM_MATERIAL);
         glPushMatrix();
             glTranslatef(teapotX, teapotY, 0);
+
+            if(ID==2) glColor3f(1,0,0);
+            else glColor3f(1,1,1);
             if(show[2]) glmDraw(uparmR,GLM_MATERIAL);
         glPopMatrix();
+
+        if(ID==3) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[3]) glmDraw(lowarmR,GLM_MATERIAL);
     glPopMatrix();
     glutSwapBuffers();
